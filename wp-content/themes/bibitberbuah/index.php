cf095f59243454f8b987fdcce8d861d54ed75fd0
moslembusana <moslembusana@gmail.com>
2017-07-26 06:32:43 +0700

    versi 9 index tanpa kategori

--- wp-content/themes/bibitberbuah/index.php
+++ wp-content/themes/bibitberbuah/index.php
@@ -9,7 +9,7 @@
 
         $args = array(
             'post_type'=>'bibitberbuah', // Your post type name
-            'posts_per_page' => 2,
+            'posts_per_page' => 15,
             'paged' => $paged,
         );
 
